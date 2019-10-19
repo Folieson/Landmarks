b871759faac2f286aa32598d733a4ef0aeabfa08
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-19 21:06:49 +0500

    Use an Observable Object for Storage
    Section 4

---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Андрей Понамарчук on 19.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
