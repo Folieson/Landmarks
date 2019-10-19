14d990c1f34e189d3ebd81ec9a5f511fb30fcc2f
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-19 16:35:32 +0500

    Creating and Combining Views
    Creating and Combining Views tutorial completed

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Андрей Понамарчук on 19.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
