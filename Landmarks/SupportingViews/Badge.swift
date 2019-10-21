6c0abc3fe39826028df810ff1ea30f0e7808676a
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-21 20:53:05 +0500

    Create a Badge View
    Section 1

---
+++ Landmarks/SupportingViews/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Андрей Понамарчук on 21.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
