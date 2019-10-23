19633ff67ddc40981b433dedefe5b74e06d48a61
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-23 20:52:28 +0500

    Add Animations to Individual Views
    Section 1

---
+++ Landmarks/HikeView/HikeView.swift
@@ -0,0 +1,57 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Андрей Понамарчук on 23.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+                    .animation(nil)
+                
+                VStack(alignment: .leading) {
+                    Text(verbatim: hike.name)
+                        .font(.headline)
+                    Text(verbatim: hike.distanceText)
+                }
+                
+                Spacer()
+
+                Button(action: {
+                    self.showDetail.toggle()
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
+                        .padding()
+                }
+            }
+
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: hikeData[0])
+                .padding()
+            Spacer()
+        }
+    }
+}
+
