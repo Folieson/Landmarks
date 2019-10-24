04ed94f321c128380a4830c7ceb7665eb0a16caf
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-24 20:35:56 +0500

    Add Rows of Landmarks
    Section 3

---
+++ Landmarks/CategoryHome.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Андрей Понамарчук on 24.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var categories: [String:[Landmark]] {
+        Dictionary(grouping: landmarkData,
+                   by: {$0.category.rawValue})
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
---
+++ Landmarks/CategoryRow.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Андрей Понамарчук on 24.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                            .font(.headline)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
