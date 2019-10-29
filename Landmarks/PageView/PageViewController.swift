f970ccd222734ae34f0e8fbb87cbc71311b30b75
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-29 20:49:23 +0500

    Create the View Controller's Data Source
    Section 2

--- Landmarks/PageView/PageViewController.swift
+++ Landmarks/PageView/PageViewController.swift
@@ -10,12 +10,18 @@ import SwiftUI
 import UIKit
 
 struct PageViewController: UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> PageViewController.Coordinator {
+        Coordinator(self)
+    }
+    
     var controllers: [UIViewController]
     
     func makeUIViewController(context: Self.Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
 
         return pageViewController
     }
@@ -24,6 +30,34 @@ struct PageViewController: UIViewControllerRepresentable {
                                 context: UIViewControllerRepresentableContext<PageViewController>) {
         pageViewController.setViewControllers([controllers[0]], direction: .forward,
                                               animated: true)
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return parent.controllers.last
+            }
+            return parent.controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            return parent.controllers[index + 1]
+        }
+        
+        var parent: PageViewController
+        
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+        }
     }
 }
 
