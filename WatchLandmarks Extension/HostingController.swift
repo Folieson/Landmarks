218ed84e3587be520cb869e178d43c7ccc7096cc
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-31 20:32:20 +0500

    Add a watchOS Target
    Section 1

---
+++ WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by Андрей Понамарчук on 31.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Андрей Понамарчук on 31.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
